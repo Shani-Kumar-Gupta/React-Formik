a78d62926aee82fe973ae9879d6d45e7d747dc31
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2020-11-11 23:34:29 +0530

    pass initial value of form field

--- react-formik-simple-form/src/components/SimpleForm.js
+++ react-formik-simple-form/src/components/SimpleForm.js
@@ -3,19 +3,28 @@ import {useFormik} from 'formik';
 
 function SimpleForm() {
 
-    const formik = useFormik({})
+    // called useFormik hook passing in an object and return object
+    // Step 1: pass initial value of form field
+    // Step 2: add onChange and value prop for each of the form field
+    const formik = useFormik({
+        initialValues : {
+            name : '',
+            email: '',
+            channel : ''
+        }
+    }) 
 
     return (
         <div>
             <form>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' id='name' name='name'/>
+                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
 
                 <label htmlFor='email'>E-mail:</label>
-                <input type='email' id='email' name='email'/>
+                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
 
                 <label htmlFor='channel'>Channel:</label>
-                <input type='text' id='channel' name='channel'/>
+                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
 
                 <button>Submit</button>
             </form>
