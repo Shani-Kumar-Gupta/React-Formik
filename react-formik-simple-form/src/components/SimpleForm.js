b9234e2c045773fb09bf71f306951d7afe2f8294
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2020-11-12 23:11:00 +0530

    added formik validation

--- react-formik-simple-form/src/components/SimpleForm.js
+++ react-formik-simple-form/src/components/SimpleForm.js
@@ -1,5 +1,36 @@
 import React from 'react';
-import {useFormik} from 'formik';
+import { useFormik } from 'formik';
+
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+
+const onSubmit = values => {
+    console.log("Form Data", values);
+}
+
+const validate = values => {
+    // values.name values.email values.channel
+    // errors.name errors.email errors.channel
+    // errors.name = 'This field is Required'
+    let errors = {}
+
+    if (!values.name) {
+        errors.name = 'Required'
+    }
+
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email format'
+    }
+
+    if (!values.channel) {
+        errors.channel = 'Required'
+    }
+}
 
 function SimpleForm() {
 
@@ -11,16 +42,10 @@ function SimpleForm() {
     //Step 1: Specify the onSubmit handler to form element
     //Step 2: Specify onSubmit property 
     const formik = useFormik({
-        initialValues : {
-            name : '',
-            email: '',
-            channel : ''
-        },
-        onSubmit : values => {
-            console.log("Form Data",values);
-        }
-    }) 
-
+        initialValues,
+        onSubmit,
+        validate
+    })
 
     //console.log("Form Value",formik.values);
 
@@ -28,13 +53,13 @@ function SimpleForm() {
         <div>
             <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
+                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
 
                 <label htmlFor='email'>E-mail:</label>
-                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
+                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
 
                 <label htmlFor='channel'>Channel:</label>
-                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
 
                 <button type="submit">Submit</button>
             </form>
